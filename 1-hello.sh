6e2d53d26c5b560e1781e7e47bc229ff37b5c184
Jian Lao <laojian.pku@gmail.com>
2024-04-10 17:02:55 -0400

    Variables...

--- 1-hello.sh
+++ 1-hello.sh
@@ -1,2 +1,28 @@
 #! /bin/bash
 echo "Hello World"
+
+# System variables
+# echo, not ECHO
+echo $BASH
+echo $bash # This will output a empty line, not $BASH info.
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
+
+# User defined variables
+name=Mark
+# Below 2 lines are the same.
+echo The name is $name
+echo "The name is $name"
+
+10val=10
+echo Value is $10val
+# Output is "Value is 0val" since variable name should not start with number.
+
+val=10
+echo Value is $val
+# Output is "Value is 10"
+
+VALUE=10
+echo Value is $VALUE
+# Output is "Value is 10"
